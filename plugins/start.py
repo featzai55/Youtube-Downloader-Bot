47a72d12ea89bf8b0c59fcf91849a788d3984619
featzai55 <77040252+featzai55@users.noreply.github.com>
2021-03-19 23:23:13 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -7,8 +7,8 @@ async def start(client, message):
     joinButton = InlineKeyboardMarkup([
         [InlineKeyboardButton("Owner", url="https://t.me/featzai")],
         [InlineKeyboardButton(
-            "Url Uploader", url="https://t.me/Ftz4i")]
+            "Url Uploader", url="https://t.me/Ftz4ibot")]
     ])
-    welcomed = f"Namaste <b>{message.from_user.first_name} We are made for goodness 😌🍉 </b>\n/help for More info"
+    welcomed = f"Namaste <b>{message.from_user.first_name}</b> We are made for goodness 😌🍉 \n/help for More info"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
